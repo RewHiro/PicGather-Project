f9daeb39fd6fbe3d0fd5db09a99c76d0bcdb2166
syougun360 <syougun360@infoseek.jp>
2015-01-09 00:55:04 +0900

    #81 音(SE)をエフェクトとして鳴らす(仮SE)

---
+++ PicGather/Assets/Effect/Graphic/EffectController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject GraphicEffectPrefab = null;
+
+    [SerializeField]
+    private GameObject SoundEffectPrefab = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    bool IsMouseButtonDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        if (TouchManager.IsTouching(this.gameObject) || IsMouseButtonDown())
+        {
+            Instantiate(GraphicEffectPrefab, this.gameObject.transform.position, Quaternion.identity);
+            Instantiate(SoundEffectPrefab, this.gameObject.transform.position, Quaternion.identity);
+        }
+
+	}
+}
---
+++ PicGather/Assets/SoundEffectController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffectController : MonoBehaviour {
+
+    [SerializeField]
+    AudioClip AudioEffectClip = null;
+    AudioSource AudioSource;
+
+	// Use this for initialization
+	void Start () {
+        AudioSource = GetComponent<AudioSource>();
+        AudioSource.clip = AudioEffectClip;
+        AudioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!AudioSource.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
