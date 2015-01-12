a0f74d5e01c43f53ff3be5aac85af7ebb593b152
syougun360 <syougun360@infoseek.jp>
2015-01-12 19:44:31 +0900

    [修正] TouchManagerにMouse処理を追加

--- PicGather/Assets/Leaf/LeafStampController.cs
+++ PicGather/Assets/Leaf/LeafStampController.cs
@@ -12,58 +12,19 @@ public class LeafStampController : MonoBehaviour {
     [SerializeField]
     GameObject LeafPrefab = null;
 
-    Vector3 TapPos = Vector3.zero;
-
+    [SerializeField]
+    GameObject TreeObject = null;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-    /// <summary>
-    /// デバックで使用
-    /// マウス左ボタンが押されてるときの処理
-    /// Rayを飛ばして当たってたら、trueを返す
-    /// </summary>
-    /// <returns>当たったかどうか?の判定</returns>
-    bool IsMouseButton()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit = new RaycastHit();
-            if(IsRayCatsHit(ray,hit))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /// <summary>
-    /// RayCastで当たったかどうかの判定
-    /// タップした座標を取得する。
-    /// </summary>
-    /// <param name="ray"></param>
-    /// <param name="hit"></param>
-    /// <returns>当たったかどうか</returns>
-    bool IsRayCatsHit(Ray ray,RaycastHit hit)
-    {
-        if (Physics.Raycast(ray, out hit))
-        {
-            if (hit.collider.gameObject.name == "Tree")
-            {
-                TapPos = new Vector3(hit.point.x, hit.point.y, hit.point.z);
-                return true;
-            }
-        }
-        return false;
-    }
-
+    
 	void Update () 
     {
-        if (IsMouseButton())
+        if (TouchManager.IsTouching(TreeObject))
         {
-            Instantiate(LeafPrefab, TapPos, Quaternion.identity);
+            Instantiate(LeafPrefab, TouchManager.TapPos, Quaternion.identity);
         }
 	}
 }
--- PicGather/Assets/TouchManager/TouchManager.cs
+++ PicGather/Assets/TouchManager/TouchManager.cs
@@ -5,6 +5,7 @@ public class TouchManager : MonoBehaviour {
 
     public static bool IsPhaseTap { get; private set; }
     public static bool IsPhaseSwipe { get; private set; }
+    public static Vector3 TapPos {get;private set;}
 
 	// Use this for initialization
 	void Start () 
@@ -17,9 +18,24 @@ public class TouchManager : MonoBehaviour {
 
 	}
 
-    public static bool IsTouching(GameObject gameObject_ )
+    static void SetNonPhase()
     {
         IsPhaseTap = IsPhaseSwipe = false;
+    }
+
+
+    static void TouchPhaseJudgment(Touch touch)
+    {
+        if (touch.phase == TouchPhase.Began) IsPhaseTap = true;
+        if (touch.phase == TouchPhase.Moved) IsPhaseSwipe = true;
+    }
+
+    public static bool IsTouching(GameObject gameObject_ )
+    {
+        SetNonPhase();
+
+        if (IsMouseButton(gameObject_)) return true;
+        if (IsMouseButtonDown(gameObject_)) return true;
 
         foreach (var touch in Input.touches)
         {
@@ -28,19 +44,94 @@ public class TouchManager : MonoBehaviour {
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 RaycastHit hit = new RaycastHit();
 
-                if (Physics.Raycast(ray, out hit))
+                if (IsRayCatsHit(ray, hit,touch, gameObject_))
                 {
-                    if (touch.phase == TouchPhase.Began) IsPhaseTap = true;
-                    if (touch.phase == TouchPhase.Moved) IsPhaseSwipe = true;
-
-                    if(hit.collider.gameObject == gameObject_)
-                    {
-                        return true;
-                    }
+                    return true;
                 }
             }
         }
         return false;
     }
 
+    /// <summary>
+    /// デバックで使用
+    /// マウス左ボタンが押されてるときの処理
+    /// Rayを飛ばして当たってたら、trueを返す
+    /// </summary>
+    /// <returns>当たったかどうか?の判定</returns>
+    static bool IsMouseButton(GameObject gameObject_)
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (IsRayCatsHit(ray, hit,gameObject_))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// デバックで使用
+    /// マウス左ボタンが押されたときの処理
+    /// Rayを飛ばして当たってたら、trueを返す
+    /// </summary>
+    /// <returns>当たったかどうか?の判定</returns>
+    static bool IsMouseButtonDown(GameObject gameObject_)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (IsRayCatsHit(ray, hit, gameObject_))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// RayCastで当たったかどうかの判定
+    /// タップした座標を取得する。
+    /// </summary>
+    /// <param name="ray"></param>
+    /// <param name="hit"></param>
+    /// <returns>当たったかどうか</returns>
+    static bool IsRayCatsHit(Ray ray, RaycastHit hit,GameObject gameObject_)
+    {
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject == gameObject_)
+            {
+                TapPos = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// RayCastで当たったかどうかの判定
+    /// タップした座標を取得する。
+    /// </summary>
+    /// <param name="ray"></param>
+    /// <param name="hit"></param>
+    /// <returns>当たったかどうか</returns>
+    static bool IsRayCatsHit(Ray ray, RaycastHit hit,Touch touch, GameObject gameObject_)
+    {
+        if (Physics.Raycast(ray, out hit))
+        {
+            TouchPhaseJudgment(touch);
+
+            if (hit.collider.gameObject == gameObject_)
+            {
+                TapPos = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                return true;
+            }
+        }
+        return false;
+    }
 }
