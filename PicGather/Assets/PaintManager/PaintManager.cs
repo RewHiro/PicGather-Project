b115a1f39a62f9f7b8a9968dc2385a1205975fb2
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-01-12 16:01:33 +0900

    キャンパスの設置、マウスで描ける

---
+++ PicGather/Assets/PaintManager/PaintManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//
+//　一筆生成機能
+//
+//　HACK:フラグを用意してペイント機能を制御している->マウスイベントだけで制御する
+//
+public class PaintManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject prefab;
+
+    //　ペイントに必要な制御
+    public bool isDraw { get; private set; }
+
+    void OnMouseDown()
+    {
+        isDraw = true;
+        Instantiate(prefab, Vector3.zero, Quaternion.identity);
+    }
+
+    void OnMouseUp()
+    {
+        isDraw = false;
+    }
+}
