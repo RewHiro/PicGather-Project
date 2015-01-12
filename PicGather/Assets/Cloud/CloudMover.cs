548a181db1e374d68727c480f340bb6788f5daae
syougun360 <syougun360@infoseek.jp>
2015-01-12 17:33:58 +0900

    # 62 雲の動き実装

---
+++ PicGather/Assets/Cloud/CloudMover.cs
@@ -0,0 +1,35 @@
+﻿/// ---------------------------------------------------
+/// date ： 2015/01/12  
+/// brief ： 雲オブジェクトの移動処理 / ビルボード処理
+/// author ： Yamada Masamistu
+/// ---------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class CloudMover : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject CenterObject = null;
+
+    float Radius = 0.0f;
+    float Angle = 0.0f;
+	
+    // Use this for initialization
+	void Start () {
+        Radius = Random.Range(-5, -3);
+	}
+	
+	void Update () 
+    {
+        var pos = CenterObject.transform.position;
+
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.back);
+
+        transform.rotation *= Quaternion.Euler(0, 180, 0);
+
+        transform.position = new Vector3(pos.x + Mathf.Cos(Angle) * Radius,
+                                pos.y, pos.z + Mathf.Sin(Angle) * Radius);
+        Angle += 0.01f;
+	}
+}
