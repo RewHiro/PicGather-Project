f0c69dbea7ad5daaeb3d5e2478e08e34fec1588b
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-01-16 01:08:52 +0900

    修正

---
+++ PicGather/Assets/PaintManager/LineClearner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ゲームオブジェクトを削除する機能
+/// </summary>
+
+public class LineClearner : MonoBehaviour
+{
+
+    public void ClearLineByTag()
+    {
+        var clones = GameObject.FindGameObjectsWithTag("Line");
+        if (clones == null) return;
+        foreach (var clone in clones)
+        {
+            Destroy(clone);
+        }
+    }
+}
