da933a44848e914354a5a95b52dcd72e1a296280
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-01-15 14:37:31 +0900

    修正とテンプレートの選択実装

---
+++ PicGather/Assets/PaintManager/GameObjectCleaner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ゲームオブジェクトを削除する機能
+/// </summary>
+
+public class GameObjectCleaner : MonoBehaviour
+{
+
+    public void ClearGameObjectByTag(string nameTag)
+    {
+        var clones = GameObject.FindGameObjectsWithTag(nameTag);
+        if (clones == null) return;
+        foreach (var clone in clones)
+        {
+            Destroy(clone);
+        }
+    }
+}
