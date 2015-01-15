6ec31ad5be3f2afecee2d7ef09a7ab8178aa8b52
syougun360 <syougun360@infoseek.jp>
2015-01-15 23:36:27 +0900

    #133 Stampできる葉っぱを選択し、それを生成できるようにする。

---
+++ PicGather/Assets/Leaf/StampListMover.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StampListMover : MonoBehaviour {
+
+    [SerializeField]
+    AnimationClip OpenAnimClip = null;
+
+    [SerializeField]
+    AnimationClip CloseAnimClip = null;
+
+    Animation MoveAnimation = null;
+
+    enum State
+    {
+        Open,
+        Close,
+    };
+
+    State state = State.Close;
+
+    // Use this for initialization
+	void Start () {
+        MoveAnimation = GetComponent<Animation>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void Open()
+    {
+        if (state == State.Open) return;
+
+        state = State.Open;
+        MoveAnimation.PlayQueued(OpenAnimClip.name);
+    }
+
+    public void Close()
+    {
+        if (state == State.Close) return;
+
+        state = State.Close;
+        MoveAnimation.PlayQueued(CloseAnimClip.name);
+    }
+
+}
