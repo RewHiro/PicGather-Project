c7d01d80ef2b131599805cc4334d88142c73a90a
sgmt <doku0911@hotmail.co.jp>
2015-01-22 00:50:22 +0900

    #77について修正しました

---
+++ PicGather/Assets/FerverGauge/FeverGaugeDirector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeverGaugeDirector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        ///常にカメラの方向を向くようにする。
+        transform.LookAt(Camera.main.transform.position);
+
+        FeverManager.AddScore(3);
+
+        ///FeverScoreの割合分だけ回転させる。
+        transform.Rotate(-180 * (FeverManager.FeverScore * 1.0f / FeverManager.MaxFeverScore), 0, 0);
+
+	}
+}
