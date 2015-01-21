39936d20c0441bacb46c61729bc048cbce661b62
sgmt <doku0911@hotmail.co.jp>
2015-01-22 01:06:40 +0900

    #77:スクリプト上で不要なものを消しました。

--- PicGather/Assets/FerverGauge/FeverGaugeDirector.cs
+++ PicGather/Assets/FerverGauge/FeverGaugeDirector.cs
@@ -14,8 +14,6 @@ public class FeverGaugeDirector : MonoBehaviour {
         ///常にカメラの方向を向くようにする。
         transform.LookAt(Camera.main.transform.position);
 
-        FeverManager.AddScore(3);
-
         ///FeverScoreの割合分だけ回転させる。
         transform.Rotate(-180 * (FeverManager.FeverScore * 1.0f / FeverManager.MaxFeverScore), 0, 0);
 
