3eb43284a06a92bdd51cf24e7a14e8fbee99c9cd
syougun360 <syougun360@infoseek.jp>
2015-01-25 00:20:54 +0900

    シャボン玉がお絵かきモード以外ならMeshを描画しないようにする様にした。

---
+++ PicGather/Assets/Babble/BabbleRendererController.cs
@@ -0,0 +1,53 @@
+﻿/// ---------------------------------------------------
+/// date ： 2015/01/25 
+/// brief ： シャボン玉のMeshの描画のコントローラー
+/// author ： Yamada Masamistu
+/// ---------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class BabbleRendererController : MonoBehaviour {
+
+    [SerializeField]
+    DrawingCanvasSlider Slider = null;
+
+    MeshRenderer Renderer = null;
+
+	// Use this for initialization
+	void Start () {
+	    Renderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ModeManager.IsDrawingMode())
+        {
+            Renderer.enabled = false;
+        }
+        Enabled();
+	}
+
+    /// <summary>
+    /// お絵かきモードのCloseボタンを押したら、描画可能にする。
+    /// </summary>
+    void Enabled()
+    {
+        if (Slider.IsCloseOnClick())
+        {
+            StartCoroutine("WaitEnabled");
+        }
+
+    }
+
+    /// <summary>
+    /// 少し待ってから描画可能にする。
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator WaitEnabled()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        Renderer.enabled = true;
+    }
+}
