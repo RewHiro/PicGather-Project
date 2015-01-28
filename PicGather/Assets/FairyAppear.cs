f9b3bc34355b610e77866ac0f36db039740b8cf8
syougun360 <syougun360@infoseek.jp>
2015-01-28 23:36:47 +0900

    絵を描いて保存すると妖精が現れるようにした

---
+++ PicGather/Assets/FairyAppear.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FairyAppear : MonoBehaviour {
+
+    [SerializeField]
+    string TreeName = "Tree";
+
+    bool IsMove = true;
+
+    const float DownSpeed = -1.0f;
+
+	// Use this for initialization
+	void Start () {
+        var TreeObject = GameObject.Find(TreeName);
+        var Position = TreeObject.transform.position;
+        var Scale = TreeObject.transform.lossyScale*3;
+
+        var AppearancePosX = Position.x + Random.Range(-Scale.x, Scale.x);
+        var AppearancePosZ = Position.z + Random.Range(-Scale.z, Scale.z);
+
+        transform.position = new Vector3(AppearancePosX, Position.y + 10, AppearancePosZ);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!IsMove) return;
+
+        transform.Translate(new Vector3(0, DownSpeed * Time.deltaTime, 0));
+	}
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == TreeName)
+        {
+            IsMove = false;
+        }
+    }
+}
