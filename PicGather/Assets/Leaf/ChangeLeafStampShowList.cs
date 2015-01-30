6c9ac31bb3ae75e36c4f0e36845cc819ffb80221
syougun360 <syougun360@infoseek.jp>
2015-01-30 16:41:35 +0900

    PullRequest #65 マージ

--- PicGather/Assets/Leaf/ChangeLeafStampShowList.cs
+++ PicGather/Assets/Leaf/ChangeLeafStampShowList.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
 public class ChangeLeafStampShowList : MonoBehaviour
 {
+
+    /// <summary>
+    /// Resouces内の画像データパス
+    /// </summary>
+    [SerializeField]
+    LeafStampManagerController GraphicsPath = null;
+
     /// <summary>
     /// スクロールしない場合...None する場合...RightかLeftで方向を示す
     /// </summary>
@@ -35,30 +43,38 @@ public class ChangeLeafStampShowList : MonoBehaviour
     /// スタンプのリストがいくつスクロールされているのかのカウント
     /// </summary>
     private int ScrollValue = 0;
-    
-    /// <summary>
-    /// 画像が保存されているのパス
-    /// </summary>
-    private const string GraphicsPath = "Leaf/";
-
-
 
     // Use this for initialization
     void Start()
     {
         /// 開始時に表示するスタンプ一覧を用意する
-        IconReset();
+        IconsInitialize(CheckGraphicsNumber());
+
     }
 
-
+    /// <summary>
+    /// アイコンの数が増加時、不明時に呼び、有効な数だけアイコンを初期化する
+    /// </summary>
+    /// <param name="canReadNumber">アイコンがある数を渡す</param>
+    private void IconsInitialize(int canReadNumber = MaxStampIconNumber)
+    {
+        IconsReset(canReadNumber);
+        ButtonsReset(canReadNumber);
+    }
 
 
     // Update is called once per frame
     void Update()
     {
+        /// リストに表示する最大数以下の画像数だった場合
+        if (CheckGraphicsNumber() < MaxStampIconNumber)
+        {
+            ChangeMode = ChangeModeID.None;
+            return;
+        }
 
         /// スクロールするかどうかをswitchで判断する
-        switch(ChangeMode)
+        switch (ChangeMode)
         {
             case ChangeModeID.Left:
                 IncreaceScrollValue();
@@ -71,11 +87,11 @@ public class ChangeLeafStampShowList : MonoBehaviour
         }
 
         /// スクロールされているかどうか
-        if(ChangeMode != ChangeModeID.None)
+        if (ChangeMode != ChangeModeID.None)
         {
-            IconReset();
+            IconsInitialize();
         }
-        
+
         /// 一通りの処理が終えたのでスクロールしないようにする
         ChangeMode = ChangeModeID.None;
 
@@ -87,14 +103,40 @@ public class ChangeLeafStampShowList : MonoBehaviour
     /// <summary>
     /// スタンプのアイコンを更新する
     /// </summary>
-    private void IconReset()
+    /// <param name="canReadNumber">アイコンがある数を渡す</param>
+    private void IconsReset(int canReadNumber = MaxStampIconNumber)
     {
-        for (int i = 0; i < MaxStampIconNumber; i++)
+        if (canReadNumber >= MaxStampIconNumber)
         {
-            SetLeafSprite(Leaf[i], GraphicsPath + (ScrollValue + (i + 1)));
+            canReadNumber = MaxStampIconNumber;
+        }
+
+        for (int i = 0; i < canReadNumber; i++)
+        {
+            SetLeafSprite(Leaf[i], GraphicsPath.Name + "/" + (ScrollValue + (i + 1)));
         }
     }
 
+
+    /// <summary>
+    /// スタンプアイコンがあるかどうかをチェックして
+    /// 無い場合はボタンを無効化する
+    /// </summary>
+    /// <param name="canReadNumber">アイコンがある数を渡す</param>
+    private void ButtonsReset(int canReadNumber = MaxStampIconNumber)
+    {
+        for (int i = 0; i < MaxStampIconNumber; i++)
+        {
+            if (i < canReadNumber)
+            {
+                SetButtonEnabled(Leaf[i], true);
+            }
+            else
+            {
+                SetButtonEnabled(Leaf[i], false);
+            }
+        }
+    }
 
 
 
@@ -103,13 +145,26 @@ public class ChangeLeafStampShowList : MonoBehaviour
     /// </summary>
     /// <param name="_gameObject">変更される葉</param>
     /// <param name="textureName">変更するテクスチャのパス</param>
-    private void SetLeafSprite(GameObject _gameObject,string textureName)
+    private void SetLeafSprite(GameObject _gameObject, string textureName)
     {
         var MainImage = _gameObject.GetComponent<Image>();
         var NewTexture = Resources.Load(textureName) as Texture2D;
         var NewSprite = Sprite.Create(NewTexture, new Rect(0, 0, NewTexture.width, NewTexture.height), Vector2.zero);
         MainImage.sprite = NewSprite;
     }
+
+
+    /// <summary>
+    /// Buttonの有効化、無効化をする
+    /// </summary>
+    /// <param name="_gameObject">変化を与えるGameObject</param>
+    /// <param name="isEnabled">有効...true 無効...false</param>
+    private void SetButtonEnabled(GameObject _gameObject, bool isEnabled)
+    {
+        var button = _gameObject.GetComponent<Button>();
+        button.enabled = isEnabled;
+    }
+
 
 
 
@@ -142,7 +197,7 @@ public class ChangeLeafStampShowList : MonoBehaviour
     /// </summary>
     private void IncreaceScrollValue()
     {
-        var graphic = Resources.Load(GraphicsPath + (ScrollValue + MaxStampIconNumber + 1)) as Texture2D;
+        var graphic = Resources.Load(GraphicsPath.Name + "/" + (ScrollValue + MaxStampIconNumber + 1)) as Texture2D;
         if (graphic)
         {
             ScrollValue++;
@@ -154,7 +209,6 @@ public class ChangeLeafStampShowList : MonoBehaviour
     /// <summary>
     /// リストのスクロールしている値を減少させる
     /// </summary>
-    /// <param name="decreaceValue">減少させる値(正の値で減算されます)</param>
     private void DecreaceScrollValue()
     {
         /// マイナスの値にはいかない
@@ -168,4 +222,15 @@ public class ChangeLeafStampShowList : MonoBehaviour
         }
 
     }
+
+    /// <summary>
+    /// フォルダ内の画像ファイルを探し、その数を返す
+    /// </summary>
+    private int CheckGraphicsNumber()
+    {
+        var gameObjectArray = Resources.LoadAll(GraphicsPath.Name + "/");
+
+        return gameObjectArray.Length;
+    }
+
 }
