1a5f6dbc0328650cd537d78616d2f381c8aaae0a
sgmt <doku0911@hotmail.co.jp>
2015-02-03 00:11:48 +0900

    #193カメラをマウスで回転できるようにする

--- PicGather/Assets/Camera/CameraMover.cs
+++ PicGather/Assets/Camera/CameraMover.cs
@@ -12,8 +12,7 @@ public class CameraMover : MonoBehaviour {
     /// <summary>
     /// カメラの移動量
     /// </summary>
-    [SerializeField]
-    private float MoveValue = 1.0f;
+    private float MoveValue = 2.0f;
 
     /// <summary>
     /// BabbleDestroyerクラスのDestroyAllBabblesを呼ぶために用意する
@@ -34,7 +33,7 @@ public class CameraMover : MonoBehaviour {
 	void Update () {
         if (ModeManager.IsDrawingMode) return;
 
-        if (Input.touchCount > 0) TurnCamera();
+        TurnCamera();
 	}
 
     /// <summary>
@@ -42,21 +41,64 @@ public class CameraMover : MonoBehaviour {
     /// </summary>
     private void TurnCamera()
     {
-        if (IsTouchingNothing() && Input.GetTouch(0).phase == TouchPhase.Began)
+
+        TurnByMouse();
+
+        TurnByTouch();
+
+        StopTurningCamera();
+    }
+
+    /// <summary>
+    ///タッチもマウス入力もされていなかったらカメラを回せないようにする。
+    /// </summary>
+    private void StopTurningCamera()
+    {
+        if (Input.touchCount == 0 && !Input.GetMouseButton(0)) CanMoveCamera = false;
+
+    }
+
+
+    /// <summary>
+    /// マウスによるカメラの移動
+    /// </summary>
+    private void TurnByMouse()
+    {
+        if (Input.GetMouseButtonDown(0) && IsTouchingNothing())
         {
             CanMoveCamera = true;
         }
 
-        if (CanMoveCamera && Input.GetTouch(0).phase == TouchPhase.Moved)
+        if (CanMoveCamera && Input.GetMouseButton(0))
         {
-            Camera.main.transform.RotateAround(CenterObject.localPosition, transform.up, MoveValue * Input.GetTouch(0).deltaPosition.x);
+            var deltaMouseX = Input.GetAxis("Mouse X");
+            Camera.main.transform.RotateAround(CenterObject.localPosition, transform.up, MoveValue * deltaMouseX);
 
             BblDestroyer.DestroyAllBabbles();
         }
 
-        if (!IsTouchingNothing()) CanMoveCamera = false;
+    }
 
+    /// <summary>
+    /// タッチによるカメラの移動
+    /// </summary>
+    private void TurnByTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (IsTouchingNothing() && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                CanMoveCamera = true;
+            }
 
+            if (CanMoveCamera && Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                Camera.main.transform.RotateAround(CenterObject.localPosition, transform.up, Input.GetTouch(0).deltaPosition.x);
+
+                BblDestroyer.DestroyAllBabbles();
+            }
+
+        }
     }
 
     /// <summary>
@@ -69,18 +111,35 @@ public class CameraMover : MonoBehaviour {
 
         foreach(var obj in objects)
         {
+            if (TouchManager.IsMouseButton(obj)) return false;
             if (TouchManager.IsTouching(obj)) return false;
         }
 
+        if (IsBabbleInside(Input.mousePosition)) return false;
+
+        foreach(var touch in Input.touches)
+        {
+            if (IsBabbleInside(touch.position)) return false;
+        }
+
+        return true;
+    }
+    
+    /// <summary>
+    /// 引数の座標がシャボン玉内にあるかどうかを返す
+    /// </summary>
+    /// <param name="point">調べる座標</param>
+    /// <returns>内部にある...true 外にある...false</returns>
+    private bool IsBabbleInside(Vector2 point)
+    {
         var babbles = GameObject.FindGameObjectsWithTag("Babble");
 
-
-        foreach(var babble in babbles)
+        foreach (var babble in babbles)
         {
             var PositionInScreen = new Vector3(babble.transform.position.x, babble.transform.position.y);
             PositionInScreen = Camera.main.WorldToScreenPoint(PositionInScreen);
-            var DistanceX = PositionInScreen.x - Input.GetTouch(0).position.x;
-            var DistanceY = PositionInScreen.y - Input.GetTouch(0).position.y;
+            var DistanceX = PositionInScreen.x - point.x;
+            var DistanceY = PositionInScreen.y - point.y;
 
             var DistanceXY = Mathf.Sqrt(DistanceX * DistanceX + DistanceY * DistanceY);
 
@@ -88,8 +147,7 @@ public class CameraMover : MonoBehaviour {
 
         }
 
-        return true;
+        return false;
     }
-
 }
 
