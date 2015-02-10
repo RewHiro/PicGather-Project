6f897b7f10ccd8d3e40454f5426b7827d3112f00
sgmt <doku0911@hotmail.co.jp>
2015-02-10 19:30:11 +0900

    イベントのマネージャ、スクリプトの継承元を用意

---
+++ PicGather/Assets/Event/EventStarterBase.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// イベントのスタートを管理するスクリプトの継承元
+/// </summary>
+public class EventStarterBase : MonoBehaviour {
+
+
+    /// <summary>
+    /// クラス固有のイベントを所持する
+    /// </summary>
+    [SerializeField]
+    protected GameObject OriginEventPrefab = null;
+
+    /// <summary>
+    /// EventManagerを得る
+    /// </summary>
+    protected EventManager EventMngr = null;
+
+    // Use this for initialization
+	void Start () {
+        EventMngr = GetComponent<EventManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+  
+    /// <summary>
+    /// イベントを開始する
+    /// </summary>
+    protected virtual void BeginEvent()
+    {
+    }
+}
