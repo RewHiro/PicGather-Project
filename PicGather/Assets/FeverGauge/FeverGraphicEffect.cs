87bd06027e628aa8ba3e7e65e2774a215808f7b9
syougun360 <syougun360@infoseek.jp>
2015-02-16 20:26:38 +0900

    音符のGameObjectを作成

---
+++ PicGather/Assets/FeverGauge/FeverGraphicEffect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FeverGraphicEffect : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Prefab = null;
+
+    [SerializeField]
+    List<Texture> NoteSprite = new List<Texture>();
+
+    float Count = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ModeManager.IsFerverMode) return;
+
+        Count += Time.deltaTime;
+        if (Count >= 1)
+        {
+            var clone = (GameObject)Instantiate(Prefab, transform.position, transform.rotation);
+            clone.transform.parent = transform;
+            var index = Random.Range(0, NoteSprite.Count);
+            clone.renderer.material.mainTexture = NoteSprite[index];
+        }
+
+	}
+}
