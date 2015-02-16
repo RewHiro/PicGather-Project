335f6af455abe0f530a421ed654ab7b640edaec4
syougun360 <syougun360@infoseek.jp>
2015-02-16 22:53:02 +0900

    #216 フィーバーゲージMAX

--- PicGather/Assets/FeverGauge/FeverGraphicEffect.cs
+++ PicGather/Assets/FeverGauge/FeverGraphicEffect.cs
@@ -10,23 +10,29 @@ public class FeverGraphicEffect : MonoBehaviour {
     [SerializeField]
     List<Texture> NoteSprite = new List<Texture>();
 
+    [SerializeField]
+    float CreateTime = 0.5f;
+
     float Count = 0;
 	// Use this for initialization
 	void Start () {
-	
+        Count = CreateTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (ModeManager.IsFerverMode) return;
+        if (!ModeManager.IsFerverMode) return;
 
         Count += Time.deltaTime;
-        if (Count >= 1)
+        if (Count >= CreateTime)
         {
             var clone = (GameObject)Instantiate(Prefab, transform.position, transform.rotation);
-            clone.transform.parent = transform;
             var index = Random.Range(0, NoteSprite.Count);
+            
+            clone.transform.parent = transform;
             clone.renderer.material.mainTexture = NoteSprite[index];
+
+            Count = 0;
         }
 
 	}
