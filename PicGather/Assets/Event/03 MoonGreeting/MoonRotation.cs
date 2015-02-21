be99b3c6f4ab1c453c779870a643feff90ea6cb5
syougun360 <syougun360@infoseek.jp>
2015-02-21 12:35:07 +0900

    PullRequest#109 マージ

---
+++ PicGather/Assets/Event/03 MoonGreeting/MoonRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoonRotation : MonoBehaviour {
+
+    [SerializeField]
+    float rotationValue = 0.1f;
+
+    float rotate = 0;
+    float swingValue = 10;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotate += rotationValue * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, swingValue * Mathf.Sin(rotate)));
+	}
+}
